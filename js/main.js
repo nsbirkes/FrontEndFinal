28f67ee72bb72604b389880ca2805cd7d6cd340c
nbirkes28 <nsbirkes@gmail.com>
2025-11-12 08:50:27 -0600

    Added functions 4 - 7. Need to fix 6. Not working for the last test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// TODO fix 6: The function addButtonListeners should add a click listener that 
+// calls the toggleComments function to each button element found within the main element.
+
+// TODO 8 through 21
+
 // 1
 function createElemWithText(elemType = "p", textContent = "", className) {
     const element = document.createElement(elemType);
@@ -6,7 +11,6 @@ function createElemWithText(elemType = "p", textContent = "", className) {
 
     return element;
 }
-
 
 // 2
 function createSelectOptions(users) {
@@ -24,7 +28,6 @@ function createSelectOptions(users) {
     return options;
 }
 
-
 // 3
 function toggleCommentSection(postId) {
     if (!postId) return undefined;
@@ -36,4 +39,68 @@ function toggleCommentSection(postId) {
     section.classList.toggle("hide");
 
     return section;
+}
+
+// 4
+function toggleCommentButton(postId) {
+    if (!postId) return undefined;
+
+    const button = document.querySelector(`button[data-post-id="${postId}"]`);
+    if (!button) return null;
+
+    button.textContent = button.textContent === "Show Comments" ? "Hide Comments" : "Show Comments";
+
+    return button;
+}
+
+// 5
+function deleteChildElements(parentElement) {
+      if (!(parentElement instanceof Element)) return undefined;
+
+    let child = parentElement.lastElementChild;
+
+    while (child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+
+    return parentElement;
+}
+
+// 6 
+function addButtonListeners() {
+    const main = document.querySelector("main");
+    const buttons = main.querySelectorAll("button");
+
+    if (buttons.length === 0) return buttons;
+
+    for (const button of buttons) {
+      const postId = button.dataset.postId;
+
+      if (postId) {
+        button.addEventListener("click", (event) => {
+          toggleComments(event, postId);
+        });
+      }
+    }
+
+    return buttons;
+}
+
+// 7 
+function removeButtonListeners() {
+    const main = document.querySelector("main");
+    const buttons = main.querySelectorAll("button");
+
+    for (const button of buttons) {
+      const postId = button.dataset.postId;
+      
+      if (postId) {
+        button.removeEventListener("click", (event) => {
+          toggleComments(event, postId);
+        });
+      }
+    }
+
+    return buttons;
 }
